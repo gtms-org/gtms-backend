a98f74642bb5ad8dc88e0edf940e4483cb71cf2a
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-02-28 07:21:02 +0100

    fixes

--- jenkins/master_merge.groovy
+++ jenkins/master_merge.groovy
@@ -126,7 +126,7 @@ pipeline {
                             build job: '(GTMS Backend) Build service', wait: false, parameters: [
                                 string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
                                 string(name: 'serviceName', value: it.location.replace("${env.WORKSPACE}/packages/", "")),
-                                string(name: 'deploy', value: true),
+                                string(name: 'deploy', value: 'true'),
                                 string(name: 'DEPLOY_ENVIRONMENT', value: 'qa-master')
                             ]
                         }
