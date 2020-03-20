395c7492d08316cdaaff4112b00d9ddcdd881b97
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-03-20 16:54:02 +0100

    feat: GEOT-138: add groups service

---
+++ packages/service-groups/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "env" {}
+
+variable "tag" {}
+
+variable "jwt_secret" {}
+
+variable "jwt_refresh_token_secret" {}
+
+variable "app_domain" {}
+
+variable "queue_host" {}
+
+variable "mount_point" {}
+
+variable "docker_host" {}
+
+variable "DOCKER_REGISTRY_PASSWORD" {}
+
+variable "DOCKER_REGISTRY_USERNAME" {}
+
+
