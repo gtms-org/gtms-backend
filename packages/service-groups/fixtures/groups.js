f32d1f0f2a2bfb05f9c1efe43453dda3372e87ca
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2020-03-28 14:33:23 +0100

    feat: GEOT-129: get and update endpoints for groups service (#26)

--- packages/service-groups/fixtures/groups.js
+++ packages/service-groups/fixtures/groups.js
@@ -5,11 +5,23 @@ module.exports = [
   {
     _id: ObjectId(),
     name: 'Brudstoock',
+    slug: 'brudstoock',
     description: 'Lorem ipsum',
     type: 'default',
     visibility: 'public',
     tags: [],
     members: [],
     owner: ObjectId(),
+  },
+  {
+    _id: ObjectId(),
+    name: 'Opener',
+    slug: 'opener',
+    description: 'Lorem ipsum',
+    type: 'default',
+    visibility: 'public',
+    tags: [],
+    members: [],
+    owner: ObjectId('5e53b8e368985486d4a50921'),
   },
 ]
