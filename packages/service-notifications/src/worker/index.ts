13587de95c9e704936c6b2d08e4609ee4a2bf46b
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2020-04-25 05:03:25 +0200

    feat: GEOT-216: add retry policy to notification queue (#60)
    * feat: GEOT-216: add retry policy to notification queue
    
    * fix: GEOT-216: proper ttl for 8h delay

--- packages/service-notifications/src/worker/index.ts
+++ packages/service-notifications/src/worker/index.ts
@@ -2,23 +2,113 @@ import amqp from 'amqplib'
 import config from 'config'
 import logger from '@gtms/lib-logger'
 import { Queues, NotificationQueueMessageType } from '@gtms/commons'
+import {
+  setupRetriesPolicy,
+  IRetryPolicy,
+  getTTLExchangeName,
+} from '@gtms/client-queue'
 import { sendEmail } from './email'
 
-function processMsg(msg: amqp.Message) {
-  const json = JSON.parse(msg.content.toString())
+const retryPolicy: IRetryPolicy = {
+  queue: Queues.notifications,
+  retries: [
+    {
+      name: '30s',
+      ttl: 30000,
+    },
+    {
+      name: '15m',
+      ttl: 900000,
+    },
+    {
+      name: '1h',
+      ttl: 3600000,
+    },
+    {
+      name: '3h',
+      ttl: 10800000,
+    },
+    {
+      name: '8h',
+      ttl: 28800000,
+    },
+    {
+      name: '16h',
+      ttl: 57600000,
+    },
+  ],
+}
 
-  switch (json.type) {
-    case NotificationQueueMessageType.email:
-      return sendEmail(json.data)
+function getAttemptAndUpdatedContent(msg: amqp.ConsumeMessage) {
+  const content = JSON.parse(msg.content.toString())
+  content.tryAttempt = ++content.tryAttempt || 1
 
-    default:
-      logger.log({
-        level: 'error',
-        message: `Not supported message in notification queue ${msg.content.toString()}`,
-        traceid: json.data?.traceid,
-      })
-      break
+  return {
+    attempt: content.tryAttempt,
+    content: Buffer.from(JSON.stringify(content)),
+    traceId: content.data?.traceId,
   }
+}
+
+function sendMsgToRetry({
+  msg,
+  channel,
+  reasonOfFail,
+}: {
+  msg: amqp.ConsumeMessage
+  channel: amqp.Channel
+  reasonOfFail: Error | string
+}) {
+  const { attempt, content, traceId } = getAttemptAndUpdatedContent(msg)
+
+  if (attempt > 6) {
+    logger.log({
+      level: 'error',
+      message: `Could not process message ${content.toString()} / channel: ${
+        Queues.notifications
+      } / error: ${reasonOfFail}`,
+      traceId,
+    })
+    return
+  }
+
+  channel.publish(
+    getTTLExchangeName(Queues.notifications),
+    `retry-${attempt}`,
+    content,
+    {
+      persistent: true,
+    }
+  )
+}
+
+function processMsg(msg: amqp.Message) {
+  return new Promise((resolve, reject) => {
+    let json
+
+    try {
+      json = JSON.parse(msg.content.toString())
+    } catch (err) {
+      return reject('can not parse json')
+    }
+
+    switch (json.type) {
+      case NotificationQueueMessageType.email:
+        sendEmail(json.data)
+          .then(resolve)
+          .catch(reject)
+        break
+
+      default:
+        logger.log({
+          level: 'error',
+          message: `Not supported message in notification queue ${msg.content.toString()}`,
+          traceid: json.data?.traceid,
+        })
+        reject('not supported message type')
+        break
+    }
+  })
 }
 
 let queueConnection: amqp.Connection
@@ -30,12 +120,38 @@ export async function listenToNotificationQueue() {
       await conn.createChannel().then(ch => {
         queueConnection = conn
         const ok = ch.assertQueue(Queues.notifications, { durable: true })
-        ok.then(() => {
+        ok.then(async () => {
+          await setupRetriesPolicy(ch, retryPolicy)
           ch.prefetch(1)
         }).then(() => {
-          ch.consume(Queues.notifications, processMsg, {
-            noAck: true,
-          })
+          ch.consume(
+            Queues.notifications,
+            msg => {
+              if (msg.fields.redelivered) {
+                return sendMsgToRetry({
+                  msg,
+                  channel: ch,
+                  reasonOfFail:
+                    'Message was redelivered, so something wrong happened',
+                })
+              }
+
+              processMsg(msg)
+                .catch(err => {
+                  sendMsgToRetry({
+                    msg,
+                    channel: ch,
+                    reasonOfFail: err,
+                  })
+                })
+                .finally(() => {
+                  ch.ack(msg)
+                })
+            },
+            {
+              noAck: false,
+            }
+          )
         })
       })
     })
