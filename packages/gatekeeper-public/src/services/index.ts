8371127da8dd2a82f7f8242ce32bc48319dca9f8
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-05-07 12:52:16 +0200

    feat: master: enable tags service in public GK

--- packages/gatekeeper-public/src/services/index.ts
+++ packages/gatekeeper-public/src/services/index.ts
@@ -1,4 +1,5 @@
 import AuthService from './auth'
 import GroupsService from './groups'
+import TagsService from './tag'
 
-export default [AuthService, GroupsService]
+export default [AuthService, GroupsService, TagsService]
