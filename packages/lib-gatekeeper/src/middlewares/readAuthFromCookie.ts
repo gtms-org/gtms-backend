1ecd13b2ede81fe08becce0afef30678c33394ab
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-05-07 07:42:49 +0200

    feat: master: more logs in GK cookie middleware

--- packages/lib-gatekeeper/src/middlewares/readAuthFromCookie.ts
+++ packages/lib-gatekeeper/src/middlewares/readAuthFromCookie.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
+import logger from '@gtms/lib-logger'
 
 export const readAuthFromCookieMiddleware = (
   req: Request,
@@ -8,7 +9,20 @@ export const readAuthFromCookieMiddleware = (
   const { accessToken } = req.cookies
 
   if (accessToken && !req.headers['x-access-token']) {
+    logger.log({
+      level: 'info',
+      message: `Taking access token from cookie (${accessToken})`,
+      traceId: res.get('x-traceid'),
+    })
     req.headers['x-access-token'] = accessToken
+  }
+
+  if (!accessToken) {
+    logger.log({
+      level: 'info',
+      message: 'No access token cookie',
+      traceId: res.get('x-traceid'),
+    })
   }
 
   next()
