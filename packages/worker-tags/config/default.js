c4f3596e95ecf2f6c7e60fac0617dfcc3db4135f
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-05-17 21:43:45 +0200

    fix: master: set internal GK in config

--- packages/worker-tags/config/default.js
+++ packages/worker-tags/config/default.js
@@ -1,13 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
-const { QUEUE_HOST, VERSION, PORT, DB_HOST, APP_KEY } = process.env
+const {
+  QUEUE_HOST,
+  VERSION,
+  PORT,
+  DB_HOST,
+  APP_KEY,
+  INTERNAL_GATEKEEPER,
+} = process.env
 
 module.exports = {
   port: PORT,
   queueHost: QUEUE_HOST,
   serviceName: 'worker-tags',
   serviceVersion: VERSION,
+  internalGatekeeper: INTERNAL_GATEKEEPER,
   dbHost: DB_HOST,
   dbName: 'tags',
   appKey: APP_KEY,
