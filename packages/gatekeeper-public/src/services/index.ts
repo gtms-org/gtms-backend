1f39f9abb5c59eaadaa1c0472d4f9ec1802ce34f
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-05-23 13:43:18 +0200

    fix: master: add new config to index

--- packages/gatekeeper-public/src/services/index.ts
+++ packages/gatekeeper-public/src/services/index.ts
@@ -2,5 +2,14 @@ import AuthService from './auth'
 import GroupsService from './groups'
 import TagsService from './tag'
 import FilesService from './files'
+import PostsService from './posts'
+import CommentsService from './comments'
 
-export default [AuthService, GroupsService, TagsService, FilesService]
+export default [
+  AuthService,
+  GroupsService,
+  TagsService,
+  FilesService,
+  PostsService,
+  CommentsService,
+]
