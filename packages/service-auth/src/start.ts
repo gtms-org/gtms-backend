7cd15a9199193ab97405e1def0e625d47baae7a8
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-06-14 20:26:53 +0200

    fix: master: switch to common consul implementation

--- packages/service-auth/src/start.ts
+++ packages/service-auth/src/start.ts
@@ -5,10 +5,6 @@ import { registerInConsul } from '@gtms/lib-consul'
 
 const port = parseInt(config.get<string>('port'), 10) || 3000
 
-// require('dns').lookup(require('os').hostname(), function (_: any, add: any) {
-//   console.log('addr: '+add);
-// })
-
 app.listen(port, () => {
   logger.info(`Auth service started on port ${port}`)
 
