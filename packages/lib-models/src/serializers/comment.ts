1b67822c57a49817a2ff4c323e9d6b8bd3cba100
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-07-09 11:51:30 +0200

    fix: master: try to serialize comment in a better way

--- packages/lib-models/src/serializers/comment.ts
+++ packages/lib-models/src/serializers/comment.ts
@@ -2,14 +2,24 @@ import { IComment } from '../models/comments'
 import { ISerializedComment, ISerializedUser } from '@gtms/commons'
 
 export function serializeComment(
-  comment: IComment,
+  comment:
+    | IComment
+    | {
+        _id: string
+        subComments: any
+        text: string
+        tags: string[]
+        owner: string
+        createdAt: string
+        updatedAt: string
+      },
   owners?: { [id: string]: ISerializedUser }
 ): ISerializedComment {
   return {
     id: comment._id,
     text: comment.text,
-    subComments: (comment.subComments || []).map(subComment => ({
-      ...subComment,
+    subComments: (comment.subComments || []).map((subComment: any) => ({
+      ...serializeComment(subComment),
       owner: (owners !== undefined && owners[`${comment.owner}`]
         ? owners[`${comment.owner}`]
         : subComment.owner) as ISerializedUser,
