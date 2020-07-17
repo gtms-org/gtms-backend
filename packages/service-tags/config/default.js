a2020cfe49724d1ba198cd0b5caff2ff7b0ebf8e
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-07-17 02:33:07 +0200

    fix: master: add missing queueHost config prop

--- packages/service-tags/config/default.js
+++ packages/service-tags/config/default.js
@@ -1,11 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
-const { DB_HOST, VERSION, PORT, CONSUL_HOST, CONSUL_PORT } = process.env
+const {
+  DB_HOST,
+  VERSION,
+  PORT,
+  CONSUL_HOST,
+  CONSUL_PORT,
+  QUEUE_HOST,
+} = process.env
 
 module.exports = {
   port: PORT,
   dbHost: DB_HOST,
+  queueHost: QUEUE_HOST,
   dbName: 'tags',
   serviceName: 'tags',
   serviceVersion: VERSION,
