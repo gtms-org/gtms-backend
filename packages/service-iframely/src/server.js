3c5970f2083fd34f0365269daf5b2f1c4d350e4d
Mariusz Kabała <62866+mariusz-kabala@users.noreply.github.com>
2020-08-17 16:29:19 +0200

    feat: GEOT-424: iframely (#171)
    * feat: master: add iframely
    
    * fix: GEOT-424: proper iframely

---
+++ packages/service-iframely/src/server.js
@@ -0,0 +1,43 @@
+const sysUtils = require('./utils')
+const app = require('./app')
+const consul = require('./consul')
+
+const server = app.listen(
+  process.env.PORT || CONFIG.port,
+  process.env.HOST || CONFIG.host,
+  function() {
+    console.log(
+      '\niframely is running on ' +
+        server.address().address +
+        ':' +
+        server.address().port
+    )
+    console.log('API endpoints: /oembed and /iframely; Debugger UI: /debug\n')
+  }
+)
+
+if (CONFIG.ssl) {
+  require('https')
+    .createServer(CONFIG.ssl, app)
+    .listen(CONFIG.ssl.port)
+}
+
+console.log('')
+console.log(' - support@iframely.com - if you need help')
+console.log(' - twitter.com/iframely - news & updates')
+console.log(' - github.com/itteco/iframely - star & contribute')
+
+consul().then(() => {
+  console.log('iframely registered in consul')
+})
+
+if (!CONFIG.DEBUG) {
+  const GracefulServer = require('graceful-cluster').GracefulServer
+  new GracefulServer({
+    server: server,
+    log: sysUtils.log,
+    shutdownTimeout: CONFIG.SHUTDOWN_TIMEOUT,
+  })
+}
+
+module.exports = server
---
+++ packages/service-iframely/terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "env" {}
+
+variable "tag" {}
+
+variable "docker_host" {}
+
+variable "DOCKER_REGISTRY_PASSWORD" {}
+
+variable "DOCKER_REGISTRY_USERNAME" {}
+
+variable "instances" {
+    default = 1
+}
