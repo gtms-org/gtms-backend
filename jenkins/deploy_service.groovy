e36084cda54306bd6788378b334e4493c0d6eb28
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-08-18 07:32:27 +0200

    chore: master: add iframely to deploy script

--- jenkins/deploy_service.groovy
+++ jenkins/deploy_service.groovy
@@ -479,6 +479,32 @@ pipeline {
             }
         }
 
+        stage ('Deploy iframely') {
+            when {
+                environment name: 'SERVICE_NAME', value: 'service-iframely'
+            }
+             steps {
+                dir("packages/${env.SERVICE_NAME}/terraform") {
+                    script {
+                        docker.withRegistry('https://docker-registry.kabala.tech', 'docker-registry-credentials') {
+                            sh "terraform init"
+                            sh "terraform workspace select ${env.DEPLOY_ENVIRONMENT} || terraform workspace new ${env.DEPLOY_ENVIRONMENT}"
+                            sh "terraform plan -out deploy.plan -var-file=${env.DEPLOY_ENVIRONMENT}.tfvars -var=\"tag=${version}\" -var=\"DOCKER_REGISTRY_USERNAME=${DOCKER_REGISTRY_USERNAME}\" -var=\"DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD}\"" 
+                            sh "terraform apply -auto-approve deploy.plan"
+                        }
+                    }
+                }
+            }
+            post {
+                success {
+                    rocketSend channel: "deployments-${env.DEPLOY_ENVIRONMENT}", message: "[${BUILD_DISPLAY_NAME}] :sunny: *${env.SERVICE_NAME}* version: *${version}* Build succeeded - ${env.JOB_NAME} ${env.BUILD_NUMBER}  (<${env.BUILD_URL}|Open>)", rawMessage: true
+                }
+                unsuccessful {
+                    rocketSend channel: "deployments-${env.DEPLOY_ENVIRONMENT}", message: "[${BUILD_DISPLAY_NAME}] :sob: *${env.SERVICE_NAME}* version: *${version}* Build failed - ${env.JOB_NAME} ${env.BUILD_NUMBER}  (<${env.BUILD_URL}|Open>)", rawMessage: true
+                }
+            }
+        }
+
         stage ('Deploy swagger') {
             when {
                 environment name: 'SERVICE_NAME', value: 'swagger'
