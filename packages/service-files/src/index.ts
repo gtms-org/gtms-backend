ecf5490880a857a26afb4ca3054ff815b042ad1b
Mariusz Kabala <mariusz@kabala.waw.pl>
2020-08-26 20:50:04 +0200

    chore: GEOT-430: merge master

--- packages/service-files/src/index.ts
+++ packages/service-files/src/index.ts
@@ -23,9 +23,11 @@ router.get('/managment/heath', (_: Request, res: Response) => {
 
 router.post('/groups/logo', getCreateFileAction(FileTypes.groupLogo))
 router.post('/groups/bg', getCreateFileAction(FileTypes.groupBg))
+router.post('/groups/cover', getCreateFileAction(FileTypes.groupCover))
 router.post('/avatar', getCreateFileAction(FileTypes.avatar))
 router.post('/gallery', getCreateFileAction(FileTypes.userGallery))
 router.post('/tags/promoted', getCreateFileAction(FileTypes.groupTagLogo))
+router.post('/post/image', getCreateFileAction(FileTypes.postImage))
 
 router.all('*', (_: Request, res: Response) => {
   res.status(404).json({ status: 'not found' })
